de92a66d0713548effc451fe4c993f8d3c0597cb
martin durcan <m.durcan8@nuigalway.ie>
2017-02-25 19:16:59 +0000

    more work done

--- MartinsCode.asm
+++ MartinsCode.asm
@@ -8,10 +8,10 @@ BallUpdate:
 	JZ R6 , Up					; if direction = up, jumps to up else cotinues to down
 	
 	Down:
-		Call GetAngle 			; get angle of ball, Angle in R5
+		CALL GetAngle 			; get angle of ball, Angle in R5
 		CALL CheckAngleP45		; checks if ball angle is +45
 		JZ R6, DownP45			; if angle = +45
-		Call CheckAngleN45		; check if angle is -45
+		CALL CheckAngleN45		; check if angle is -45
 		JZ R6, DownN45			; if angle = -45
 		;else angle =vertical
 		DownVertical:			; moving Down vertical
@@ -52,28 +52,68 @@ BallUpdate:
 	;end of Down
 	
 	Up:
-		Call GetAngle 			; get angle of ball, Angle in R5
-		CALL CheckAngleP45		; checks if ball angle is +45
-		JZ R6, UpP45			; if angle = +45
-		Call CheckAngleN45		; check if angle is -45
-		JZ R6, UpN45			; if angle = -45
-		;else angle =vertical
-		UpVertical:				; moving up vertical
-			
-			
+		CALL GetAngle 		; get angle of ball, Angle in R5
+		CALL CheckAngleP45	; checks if ball angle is +45
+		JZ R6, UpP45		; if angle = +45
+		CALL CheckAngleN45	; check if angle is -45
+		JZ R6, UpN45		; if angle = -45
+		;else
+		UpVertical:			; moving up vertical
+			CALL GetYLoc		; get y location in R5
+			CALL CheckAtWall	; checks if at wall
+			JZ R6, AtWall		; if at wall jumps to AtWall 
+			;else 
 			MoveUp:				; move up
-			
+				MOVAMEMR R6, @R5	; gets the Ball from mem
+				MOVBAMEM R7, @R7	; clears the where the ball was
+				CALL IncYLoc		; increments and saves YLoc
+				MOVBAMEM R6, @R5	; saves ball new location
 			RET
-			; end MoveUp
-		;end UpVertical
-		
-		UpP45:					; moveing up at +45
 			
-			MoveUpP45:				; move up at +45
-				
+			AtWall:
+				CALL ClrWall		; clear wall
+				INVBR R0,0			; invert direction to down
+			JZ R7, MoveDown		; jumps to move down
+			; end MoveUp
+		; end UpVertical
+		
+		UpP45:				; moveing up at +45
+			CALL GetXLoc		; gets x location
+			CALL GetYLoc		; gets y location
+			CALL CheckAtTLCorner; checks if at top left corner
+			JZ R6, AtTLCorner	; if at corner jump to AtTLCorner
+			;else
+			CALL CheckAtWall	; checks if at wall
+			JZ R6, AtWallP45	; if at wall jump to AtWall
+			;else
+			CALL CheckAtLBorder	; checks if at left border
+			JZ R6, AtLBorderP45	; if at left border jump to AtLBorderP45
+			;else
+			MoveUpP45:			; move up at +45
+				MOVAMEMR R6, @R5	; gets the Ball from mem
+				MOVBAMEM R7, @R7	; clears the where the ball was
+				CALL IncYLoc		; increments and saves YLoc
+				CALL IncXLoc		; increment and save XLoc
+				SHLL R6,1			; shifts the ball left
+				MOVBAMEM R6, @R5	; saves ball new location
 			RET	
+			
+			AtTLCorner:			; at the top left corner
+				CALL ClrWall		; clear wall
+				INVBR R0,0			; invert direction to down
+			JZ R7, MoveDownP45	; jumps to move down at +45
+			
+			AtWallP45:			; at wall 
+				CALL ClrWall		; clear wall
+				INVBR R0,0			; invert direction to down
+				CALL AngleN45		; changes angle to -45
+			JZ R7, MoveDownN45	; jumps to move down at -45
+			
+			AtLBorderP45:		; at border
+				CALL AngleN45		; changes angle to -45
+			JZ R7, MoveUpN45	; jumps to move up at -45
 			; end MoveUpP45
-		;end UpP45
+		; end UpP45
 		
 		UpN45:					; moving up at -45
 			
@@ -89,6 +129,8 @@ BallUpdate:
 ;//////////////////////////////////////////////////////
 ;///////////////////	Functions	///////////////////
 ;//////////////////////////////////////////////////////
+
+;//////////////////    Getters   //////////////////////
 
 ; Get Direction of the ball
 ; 	0 for up and 1 for down
@@ -107,7 +149,7 @@ GetAngle:
 RET							; returns the angle to R5
 
 ; Get X location of ball, outputs R4
-GetYLoc:
+GetXLoc:
 	MOVRR R4, R0			; move data to R4
 	SHLL R4, 4				; shift left to remove Y location
 	SHRL R4, 12				; shift right to get X location alone
@@ -130,6 +172,91 @@ GetWall:
 	MOVAMEMR R4, @R4		; moves wall to R4
 RET							; returns the wall to R4
 	
+
+;//////////////////    Actuator   //////////////////////
+
+
+; increment X location
+IncXLoc:
+	PUSH R1
+	MOVRR R1,R4				; use R1 to remove old value
+	SHLL R1,8				; shifts to location of XLoc in R0
+	XOR R0,R0,R1			; clears old value
+	INC R4					; increment XLoc
+	SHLL R4,8				; shift to Xloc
+	ADD R0, R0, R4			; Saves new XLoc value
+	SHRL R4,8				; shift back for other functions
+	POP R1
+RET
+
+
+; Decrement X location
+DecXLoc:
+	PUSH R1
+	MOVRR R1,R4				; use R1 to remove old value
+	SHLL R1,8				; shifts to location of XLoc in R0
+	XOR R0,R0,R1			; clears old value
+	DEC R4					; Decrement XLoc
+	SHLL R4,8				; shift to Xloc
+	ADD R0, R0, R4			; Saves new XLoc value
+	SHRL R4,8				; shift back for other functions
+	POP R1
+RET
+
+
+; increment Y location
+IncYLoc:
+	PUSH R1
+	MOVRR R1,R5				; use R1 to remove old value
+	SHLL R1,12				; shifts to location of YLoc in R0
+	XOR R0,R0,R1			; clears old value
+	INC R5					; increment YLoc
+	SHLL R5,12				; shifts to location of YLoc in R0
+	ADD R0, R0, R5			; Saves new YLoc value
+	SHRL R5,12				; shift back for other functions
+	POP R1
+RET
+
+
+; Decrement Y location
+DecYLoc:
+	PUSH R1
+	MOVRR R1,R5				; use R1 to remove old value
+	SHLL R1,12				; shifts to location of YLoc in R0
+	XOR R0,R0,R1			; clears old value
+	DEC R5					; Decrement YLoc
+	SHLL R5,12				; shifts to location of YLoc in R0
+	ADD R0, R0, R5			; Saves new YLoc value
+	SHRL R5,12				; shift back for other functions
+	POP R1
+RET
+
+
+; set Angle of Ball to Vertical
+;	sets Angle = 00
+AngleVertical:
+	CLRBR R0,1
+	CLRBR R0,2
+RET
+
+
+; set Angle of Ball to +45
+;	sets Angle = 01
+AngleP45:
+	SETBR R0,1
+	CLRBR R0,2
+RET
+
+
+; set Angle of Ball to -45
+;	sets Angle = 10
+AngleN45:
+	CLRBR R0,1
+	SETBR R0,2
+RET
+
+
+;//////////////////    Func   //////////////////////	
 	
 ; Check that the Angle is +45 (01)
 ;	if it returns all 0 then its +45. else not +45
@@ -258,4 +385,11 @@ BeforeLBorderLoc:
 	ADDI R1,R1,7			;R1=0007h
 	SETBR R1,3				;R1=000Fh
 RET
-	
+
+;///////////////////// TO DO ////////////////////
+
+ClrWall:
+RET
+
+LoseLife:
+RET
