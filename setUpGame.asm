b78cc6c2d7c991ea32623aa3c4a8a0aa9ef93951
Aoife McDonagh <Aoife McDonagh>
2017-03-01 18:34:45 +0000

    basic game setup complete

--- setUpGame.asm
+++ setUpGame.asm
@@ -17,18 +17,33 @@ SETBR R4, 15
 SHRA R4, 4
 SHRL R4, 6					; R4 = 03E0h (paddle at initial location)
 XOR R5, R5, R5				; Clear R5 for use in function
-SETBR R5, 1
-SETBR R5, 2
 SETBR R5, 4					; R5 = 0010h (data memory address for paddle location)
 MOVBAMEM @R5, R4			; Move paddle position to data memory 
+
+XOR R4, R4, R4				; Clear R4 for use in function
+SETBR R4, 4
+SETBR R4, 5
+SETBR R4, 6					; R4 = 0070
+OR R0, R0, R4				; Moving the paddle location to R0
 RET
 
 initBall:
 XOR R4, R4, R4				; Clear R4 for use in function
-MOVSFRR R4, SFR5			; Move inPort value to R4 
-SHRL R4, 7					; Shift out the LED value leftover from SFR5
-SHLL R4, 7					; Shift the inPort value back to bits 15:8, preparing to move them to R0
-MOVRR R0, R4				; Move ball X and Y location to R0
+SETBR R4, 7					; Set bit to act as ball location 
+XOR R5, R5, R5				; Clear R5 for use in function
+SETBR R5, 0
+SETBR R5, 4					; R5 = 0011h (data memory address for paddle location)
+MOVBAMEM @R5, R4			; Move paddle position to data memory 
+
+XOR R4, R4, R4				; Clear R4 for use in function
+SETBR R4, 8
+SETBR R4, 9
+SETBR R4, 10				; R4 = 0700
+OR R0, R0, R4				; Moving the ball's x location to R0
+
+XOR R4, R4, R4				; Clear R4 for use in function
+SETBR R4, 12				; R4 = 1000
+OR R0, R0, R4				; Moving the ball's y location to R0
 RET
 
 initWall:
@@ -36,7 +51,8 @@ XOR R4, R4, R4				; Clear R4 for use in function
 INV R4, R4					; R4 = FFFFh
 XOR R5, R5, R5				; Clear R5 for use in function
 SETBR R5, 4
-SHRL R5, 2
+SETBR R5, 3
+SETBR R5, 2
 SETBR R5, 0					; R5 = 001Dh (memory location of wall)
 MOVBAMEM @R5, R4			; Move wall to memory
 RET
