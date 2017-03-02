6a682967ef736440464f02e4531f83c29435a61d
Aoife McDonagh <Aoife McDonagh>
2017-03-02 17:46:41 +0000

    adding push and pop statements to ClrWallBit function

--- MainCode.asm
+++ MainCode.asm
@@ -407,6 +407,8 @@ RET
 ;/////////////////	Clear Wall	///////////////////////
 ;//////////////////////////////////////////////////////
 ClrWallBit:
+PUSH R4
+PUSH R5
 XOR R4, R4, R4				; Clear R4 for use in function
 SETBR R4, 4
 SETBR R4, 3
@@ -419,6 +421,8 @@ MOVAMEMR R5, @R5			; R5 = memory address contents of ball
 INV R5, R5					; Invert ball memory address contents
 AND R5, R3, R5				; R5 = updated wall state
 MOVBAMEM @R4, R5			; Update wall state
+POP R5
+POP R4
 RET
 
 
